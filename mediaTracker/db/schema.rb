162433153b8a52db770892ff4680f3f3e86379aa
Alyssa Langhals <langhals.50@osu.edu>
2018-11-18 18:46:08 -0500

    Added simple css to deviseForms

--- mediaTracker/db/schema.rb
+++ mediaTracker/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181117040826) do
+ActiveRecord::Schema.define(version: 20181118204348) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20181117040826) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "gender"
+    t.datetime "last_log_on"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
