42ec296d5b3c7cf94bb4f2b5e461455fd31df597
Alyssa Langhals <langhals.50@osu.edu>
2018-11-20 12:34:08 -0500

    Try to add followers association

--- mediaTracker/app/models/follower.rb
+++ mediaTracker/app/models/follower.rb
@@ -1,2 +1,6 @@
 class Follower < ApplicationRecord
+
+    belongs_to :follower, foreignkey: "fTarget", class_name: "User"
+    belongs_to :followee, foreign_key: "user", class_name: "User"
+
 end
--- mediaTracker/app/models/user.rb
+++ mediaTracker/app/models/user.rb
@@ -13,4 +13,12 @@ class User < ApplicationRecord
   #Set up table association
   has_one :preference, dependent: :destroy
   has_many :history_logs, dependent: :destroy
+
+  #Associations for followers
+  has_many :followers, through: :follower_follows, source: :follower
+  has_many :follower_follows, foreign_key: :user, class_name: "Follower"
+
+  has_many :followees, through: :followee_follows, source: :followee
+  has_many :follwee_follows, foreign_key: :fTarget, class_name: "Follower"
+  #Source: https://medium.com/@jbmilgrom/active-record-many-to-many-self-join-table-e0992c27c1e
 end
