84bb855cd63653b4f76fb5bdddc5e629e2212681
Alyssa Langhals <langhals.50@osu.edu>
2018-11-27 18:33:53 -0500

    started working on the followers controller

--- mediaTracker/app/controllers/followers_controller.rb
+++ mediaTracker/app/controllers/followers_controller.rb
@@ -4,12 +4,13 @@ class FollowersController < ApplicationController
   # GET /followers
   # GET /followers.json
   def index
-    @followers = Follower.all
+    @followers = current_user.followers
   end
 
   # GET /followers/1
   # GET /followers/1.json
   def show
+    #TODO redirect to their homepage
   end
 
   # GET /followers/new
@@ -17,14 +18,11 @@ class FollowersController < ApplicationController
     @follower = Follower.new
   end
 
-  # GET /followers/1/edit
-  def edit
-  end
 
   # POST /followers
   # POST /followers.json
   def create
-    @follower = Follower.new(follower_params)
+    @follower = Follower.new(users_id: current_user.id, user: current_user.id, fTarget: params['followee'].to_i)
 
     respond_to do |format|
       if @follower.save
@@ -37,26 +35,13 @@ class FollowersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /followers/1
-  # PATCH/PUT /followers/1.json
-  def update
-    respond_to do |format|
-      if @follower.update(follower_params)
-        format.html { redirect_to @follower, notice: 'Follower was successfully updated.' }
-        format.json { render :show, status: :ok, location: @follower }
-      else
-        format.html { render :edit }
-        format.json { render json: @follower.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
+ 
   # DELETE /followers/1
   # DELETE /followers/1.json
   def destroy
     @follower.destroy
     respond_to do |format|
-      format.html { redirect_to followers_url, notice: 'Follower was successfully destroyed.' }
+      format.html { redirect_to followers_url, notice: 'User was successfully unfollowed.' }
       format.json { head :no_content }
     end
   end
@@ -69,6 +54,6 @@ class FollowersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def follower_params
-      params.require(:follower).permit(:user, :fTarget)
+      params.require(:followee)#TODO permit
     end
 end
