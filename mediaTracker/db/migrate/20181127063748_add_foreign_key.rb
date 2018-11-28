3d5e4880c7a93721cfe15a1a77a2ddc2bb08f7b7
Alyssa Langhals <langhals.50@osu.edu>
2018-11-28 16:32:11 -0500

    Added back followers foreign key

--- mediaTracker/db/migrate/20181127063748_add_foreign_key.rb
+++ mediaTracker/db/migrate/20181127063748_add_foreign_key.rb
@@ -1,5 +1,6 @@
 class AddForeignKey < ActiveRecord::Migration[5.1]
   def change
     add_foreign_key :personal_media_parameters, :users
+    add_column :followers, :users, foreign_key: true
   end
 end
