17c1f0e0a1b8cb7cd91e6c2fbdcfebd031b13744
ImBerkayKaplan <kaplanberky@gmail.com>
2018-11-28 23:52:19 -0500

    Fixed a foreign key issue that prevented the server from running

--- mediaTracker/db/migrate/20181127063748_add_foreign_key.rb
+++ mediaTracker/db/migrate/20181127063748_add_foreign_key.rb
@@ -1,6 +1,5 @@
 class AddForeignKey < ActiveRecord::Migration[5.1]
   def change
     add_foreign_key :personal_media_parameters, :users
-    add_column :followers, :users, foreign_key: true
   end
 end
