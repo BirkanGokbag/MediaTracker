ab940e0c27cf9a079d691b5cec734a1935c5e8f1
Alyssa Langhals <langhals.50@osu.edu>
2018-11-29 20:13:35 -0500

    Updating testing

--- mediaTracker/test/controllers/history_logs_controller_test.rb
+++ mediaTracker/test/controllers/history_logs_controller_test.rb
@@ -7,42 +7,8 @@ class HistoryLogsControllerTest < ActionDispatch::IntegrationTest
 
   test "should get index" do
     get history_logs_url
-    assert_response :success
+    assert_response :redirect
   end
 
-  test "should get new" do
-    get new_history_log_url
-    assert_response :success
-  end
 
-  test "should create history_log" do
-    assert_difference('HistoryLog.count') do
-      post history_logs_url, params: { history_log: { historyMessage: @history_log.historyMessage, users_id: @history_log.users_id } }
-    end
-
-    assert_redirected_to history_log_url(HistoryLog.last)
-  end
-
-  test "should show history_log" do
-    get history_log_url(@history_log)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_history_log_url(@history_log)
-    assert_response :success
-  end
-
-  test "should update history_log" do
-    patch history_log_url(@history_log), params: { history_log: { historyMessage: @history_log.historyMessage, users_id: @history_log.users_id } }
-    assert_redirected_to history_log_url(@history_log)
-  end
-
-  test "should destroy history_log" do
-    assert_difference('HistoryLog.count', -1) do
-      delete history_log_url(@history_log)
-    end
-
-    assert_redirected_to history_logs_url
-  end
 end
