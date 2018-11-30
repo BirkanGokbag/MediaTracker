984f33c4387e5828a569c9aa969afc05b74b03b8
ImBerkayKaplan <kaplanberky@gmail.com>
2018-11-30 01:13:28 -0500

    Added documentation to the files

--- mediaTracker/app/controllers/users_controller.rb
+++ mediaTracker/app/controllers/users_controller.rb
@@ -2,6 +2,8 @@
 
 #Summary: 
 class UsersController < ApplicationController
+
+    
 	def show
 	    @thisUser = User.find(params[:id])
 	    @historylog = HistoryLog.where(users_id: params[:id]).last(10)
@@ -10,17 +12,6 @@ class UsersController < ApplicationController
 	    @currUserFollowers = nil
 	    if @thisUser.id != current_user.id
 	    	@currUserFollowers = Follower.where(users_id: current_user.id)
-	    end
-
-	    # There is a limit to how many updates or followers can be displayed in the landing page
-	    @limita = @historylog.length
-	    @limitb = @followers.length
-	    @historylength = @historylog.length
-	    if @limita > 5
-	      @limita = 5
-	    end
-	    if @limitb>20
-	      @limitb = 20
 	    end
 	end
 
